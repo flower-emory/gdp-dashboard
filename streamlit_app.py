ebebf9e84263f55fd5c3bad78f33a84cba27eeef
flower-emory <emory.blackwood@gmail.com>
2024-12-02 23:09:59 +0000

    ofjhfo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,39 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-all_users = st.text_input("Movie title", "Life of Brian")
-np.random.seed(42)
-data = pd.DataFrame(np.random.randn(20, len(all_users)), columns= all_users)
+user = st.text_input("who is saving", "Larry")
 
-st.data_editor(data, height=250, use_container_width=True)
+number = st.number_input(
+    "Insert a number", value=None, placeholder="Type a number..."
+)
+st.write("The current number is ", number)
+
+st.write("Hello☺️! This is a helpful daily schedule to help you be more: Fabulously Encouraging, Lovely, Incredible, Careing, Incomprehendibly Trustworthy, Yummilicious!") 
+
+# Part 1:
+# This puts some starting values in the list of todos
+if "TODO_LIST" not in st.session_state:
+    st.session_state["TODO_LIST"] = ["Clean Room", "Check Mailbox"]
+
+# Part 2:
+# Gives you a box to write in
+# And then there's a button
+# If you push that button, it adds the thing you typed to the list
+number = st.number_input(
+    "set your gole boundrys", value=None, placeholder="Type a number..."
+)
+st.write("The current gole is ", number)
+
+if st.button("add some money"):
+    st.session_state["TODO_LIST"].append(title)
+
+
+# Part 3:
+# For each item in your list, it makes a checkbox
+for checkbox in st.session_state["TODO_LIST"]:
+    st.checkbox(checkbox)
+
+
+
+
 
