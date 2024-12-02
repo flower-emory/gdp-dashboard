edfab8149184cb42c8583ca0d155f2ded938671b
flower-emory <emory.blackwood@gmail.com>
2024-12-02 18:23:34 +0000

    bob

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-all_users = ["Allie","Emory","Elsie","Felicity","uncle z","Dad","Kate the great","Elizibith","ant Flo","mom","Lottie"]
-with st.container(border=True):
-    users = st.multiselect("Users", all_users, default=all_users)
-    rolling_average = st.toggle("Rolling average")
+
+all_users = st.text_input("Movie title", "Life of Brian")
 np.random.seed(42)
-data = pd.DataFrame(np.random.randn(20, len(users)), columns=users)
-if rolling_average:
-    data = data.rolling(7).mean().dropna()
-# tab1, tab2 = st.tabs(["Chart", "Dataframe"])
-# tab1.line_chart(data, height=250)
+data = pd.DataFrame(np.random.randn(20, len(all_users)), columns= all_users)
+
 st.data_editor(data, height=250, use_container_width=True)
 
